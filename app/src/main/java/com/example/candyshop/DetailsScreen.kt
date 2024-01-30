d691c62503fcc636670b8e9400af3057a2808037
pkuwalek <paula.kuwalek@gmail.com>
2024-01-30 16:56:46 +0100

    Cleanup.

--- app/src/main/java/com/example/candyshop/DetailsScreen.kt
+++ app/src/main/java/com/example/candyshop/DetailsScreen.kt
@@ -4,18 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,16 +31,14 @@ fun DetailsScreen(name: String?, modifier: Modifier = Modifier) {
     Column {
         if (candyItem != null) {
             Image(
-//                painter = painterResource(id = candyItem.image),
-                painter = painterResource(id = R.drawable.sprinkles),
+                painter = painterResource(id = candyItem.image),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.heightIn(max = 250.dp)
             )
         }
         Text(
-//            text = "About $name",
-            text = "About Sprinkles",
+            text = "About $name",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(start = 10.dp, top = 10.dp, end = 10.dp)
--- app/src/main/java/com/example/candyshop/Navigation.kt
+++ app/src/main/java/com/example/candyshop/Navigation.kt
@@ -1,9 +1,6 @@
 package com.example.candyshop
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
