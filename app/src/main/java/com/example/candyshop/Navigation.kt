3fe8b2a5bd09442c6bb133f87f155af30ba87e7a
pkuwalek <paula.kuwalek@gmail.com>
2024-01-31 12:35:02 +0100

    Add navigation transition animation.

--- app/src/main/java/com/example/candyshop/Navigation.kt
+++ app/src/main/java/com/example/candyshop/Navigation.kt
@@ -1,5 +1,9 @@
 package com.example.candyshop
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -10,7 +14,25 @@ import androidx.navigation.navArgument
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+    val time = 500
+    NavHost(
+        navController = navController,
+        startDestination = Screen.MainScreen.route,
+        enterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left, tween(time))
+        },
+        exitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left,tween(time))
+        },
+        popEnterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right,tween(time))
+        },
+        popExitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right,tween(time))
+        }
+
+
+    ) {
         composable(route = Screen.MainScreen.route) {
             CandyShopMain(navController = navController)
         }
