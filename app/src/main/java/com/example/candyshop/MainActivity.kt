2e3bf42fa823ae0bea17e1f9ff7b2943b26185d0
pkuwalek <paula.kuwalek@gmail.com>
2024-02-06 15:12:38 +0100

    Change MainActivity so it gets all values from Material theme or resources.

--- app/src/main/java/com/example/candyshop/MainActivity.kt
+++ app/src/main/java/com/example/candyshop/MainActivity.kt
@@ -10,32 +10,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.candyshop.ui.theme.CandyShopTheme
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
@@ -57,53 +55,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun SmallCircleImage(imageId: Int) {
+    Image(
+        painter = painterResource(id = imageId),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(dimensionResource(id = R.dimen.image_size))
+            .border(
+                dimensionResource(id = R.dimen.border_xs),
+                MaterialTheme.colorScheme.onPrimaryContainer,
+                CircleShape
+            )
+            .padding(dimensionResource(id = R.dimen.border_xs))
+            .clip(CircleShape)
+    )
+}
+
+@Composable
 fun CandyCard(name: String, imgId: Int, price: String, navController: NavController, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .padding(4.dp)
-            .fillMaxWidth()
-            .border(2.dp, Color(0xFFD3CEDF))
-            .size(width = 400.dp, height = 120.dp)
-            .padding(4.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(id = imgId),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(120.dp)
-                .border(2.dp, Color(0xFF9CB4CC), CircleShape)
-                .padding(2.dp)
-                .clip(CircleShape)
-        )
-        Column(
-            modifier = Modifier.padding(start = 16.dp)
+    Card(modifier = modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = name,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color(0xFF748DA6)
-            )
-            Text(
-                text = "price: $price",
-                color = Color(0xFF748DA6),
-                modifier = Modifier.padding(top = 6.dp, bottom = 14.dp)
-            )
-            Button(
-                onClick = {
-                    navController.navigate(Screen.DetailsScreen.withArgs(name))
-                },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFFD3CEDF),
-                contentColor = Color(0xFF748DA6)),
-            border = BorderStroke(2.dp, Color(0xFF748DA6)),
-            modifier = Modifier
-                .widthIn(min = 250.dp)
-                .heightIn(max = 35.dp)
+            SmallCircleImage(imageId = imgId)
+            Column(
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
             ) {
-                Text("DETAILS")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+                Text(
+                    text = "price: $price",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.padding_small))
+                )
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.DetailsScreen.withArgs(name))
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                        contentColor = MaterialTheme.colorScheme.onTertiaryContainer),
+                    border = BorderStroke(dimensionResource(id = R.dimen.border_xs), MaterialTheme.colorScheme.onTertiaryContainer),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("DETAILS")
+                }
             }
         }
     }
@@ -121,20 +123,19 @@ fun TopLogoBar(modifier: Modifier = Modifier) {
                 Icon(
                     painter = painterResource(id = R.drawable.icecream_icon),
                     contentDescription = "ice cream icon",
-                    tint = Color(0xFF748DA6)
+                    tint = MaterialTheme.colorScheme.onBackground
                 )
                 Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                 Text(
                     text = "CANDY SHOP",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF748DA6)
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = MaterialTheme.colorScheme.onBackground
                 )
                 Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                 Icon(
                     painter = painterResource(id = R.drawable.icecream_icon),
                     contentDescription = "ice cream icon",
-                    tint = Color(0xFF748DA6)
+                    tint = MaterialTheme.colorScheme.onBackground
                 )
             }
         }
@@ -155,7 +156,8 @@ fun CandyShopMain(navController: NavController) {
                         name = item.name,
                         imgId = item.image,
                         price = NumberFormat.getCurrencyInstance().format(item.price),
-                        navController = navController
+                        navController = navController,
+                        modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))
                     )
                 }
             }
@@ -166,5 +168,7 @@ fun CandyShopMain(navController: NavController) {
 @Preview(showBackground = true)
 @Composable
 fun CandyShopPreview() {
-    CandyShopMain(rememberNavController())
+    CandyShopTheme {
+        CandyShopMain(rememberNavController())
+    }
 }
