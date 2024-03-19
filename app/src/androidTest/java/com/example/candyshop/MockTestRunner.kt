30ab3e9b388d55e40bc6a0643c91d1322ff61db4
pkuwalek <paula.kuwalek@gmail.com>
2024-03-19 14:01:27 +0100

    Add MockTestRunner.

--- app/src/androidTest/java/com/example/candyshop/MockTestRunner.kt
+++ app/src/androidTest/java/com/example/candyshop/MockTestRunner.kt
@@ -2,13 +2,10 @@ package com.example.candyshop
 
 import android.app.Application
 import android.content.Context
-import android.os.Bundle
-import android.os.StrictMode
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
 
 class MockTestRunner : AndroidJUnitRunner() {
-
     override fun newApplication(
         cl: ClassLoader?,
         className: String?,
@@ -16,6 +13,4 @@ class MockTestRunner : AndroidJUnitRunner() {
     ): Application {
         return super.newApplication(cl, HiltTestApplication::class.java.name, context)
     }
-
-
 }
